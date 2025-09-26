3c1a23d64c9e36b558ad815a050546dcbaa48be2
Amr Kahla <amrkahla6@gmail.com>
2025-09-26 11:50:22 +0300

    Implement the guess the move function

---
+++ src/GuessTheMovie.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class GuessTheMovie {
+    private static final int MAX_WRONG_GUESSES = 10;
+    private String movieTitle;
+    private char[] guessedTitle;
+    private Set<Character> wrongGuesses = new HashSet<>();
+    private int wrongAttempts = 0;
+
+    public GuessTheMovie(String movieTitle){
+        this.movieTitle = movieTitle.toLowerCase();
+        guessedTitle    = new char[movieTitle.length()];
+
+        for (int i = 0; i < movieTitle.length(); i++) {
+            if (movieTitle.charAt(i) == ' ') {
+                guessedTitle[i] = ' '; // keep spaces
+            }else{
+                guessedTitle[i] = '_';
+            }
+        }
+    }
+}
